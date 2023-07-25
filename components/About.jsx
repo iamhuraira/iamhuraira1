0d4303808ece6cae8b1615ea6a26c58e3aa9e269
Abu Huraira <iamhuraira429@gmail.com>
2023-07-25 18:31:13 +0500

    cc

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import AboutImg from '../public/assets/boy.jpg';
+import AboutImg from '/public/assets/boy.jpg';
 
 const About = () => {
   const aboutData = {
